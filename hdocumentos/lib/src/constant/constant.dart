e90f30c0fbce21d0f9401d9bb7bf80474eaba66b
Andres <andypache@gmail.com>
2022-08-15 00:54:32 -0500

    Ip static

--- hdocumentos/lib/src/constant/constant.dart
+++ hdocumentos/lib/src/constant/constant.dart
@@ -1,6 +1,6 @@
 //config to service
 const protocol = "http";
-const host = "192.168.10.102";
+const host = "190.123.34.29";
 const baseUrl = "electronic-documents";
 //config to app create into database
 const String clientName = "b83baf55-729c-41ae-a40c-94e1c14a2c28";
